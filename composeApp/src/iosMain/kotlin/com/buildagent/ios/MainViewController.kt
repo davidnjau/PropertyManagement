05db30747d88006e0c9f451fc2f33fd685e6280b
David <davidnjau21@gmail.com>
2026-06-17 16:05:08 +0300

    fix(ios): add CADisableMinimumFrameDurationOnPhone to plist and guard startKoin against double-init

--- composeApp/src/iosMain/kotlin/com/buildagent/ios/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/buildagent/ios/MainViewController.kt
@@ -5,12 +5,17 @@ import com.buildagent.ui.App
 import com.buildagent.ui.di.appModules
 import org.koin.core.context.startKoin
 
+private var koinStarted = false
+
 fun MainViewController() = ComposeUIViewController {
-    startKoin {
-        modules(appModules(
-            baseUrl = "http://localhost:3001",
-            tokenProvider = { "" }
-        ))
+    if (!koinStarted) {
+        koinStarted = true
+        startKoin {
+            modules(appModules(
+                baseUrl = "http://localhost:3001",
+                tokenProvider = { "" }
+            ))
+        }
     }
     App()
 }
